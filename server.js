5b41beeea80411648d4ea2229d6a7705849eacf5
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-27 07:02:23 -0500

    new.ejs

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.get('/', (req, res) => {
     res.render('index', data)
 })
 
+app.get('/add', (req, res) => {
+    res.render('new')
+})
+
 app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`)
 })
