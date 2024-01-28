2cfb5e0fd51f3707f36e244f57e17ae02626c599
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-27 20:42:19 -0500

    save

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import axios from 'axios'
 import pg from 'pg'
+import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = 3000
@@ -14,19 +15,37 @@ const pgClient = new pg.Client({
 })
 await pgClient.connect()
 
-let books = await getBooksFromDB()
-
 async function getBooksFromDB() {
    const res = await pgClient.query('select * from book_reviews')
    console.log(res.rows)
    return res.rows
 }
 
+async function postBookToDB(book) {
+    const query = `insert into book_reviews (title, rating, review, img_url) values ($1, $2, $3, $4)`
+    try {
+        await pgClient.query(query, [
+            book.title, book.rating, book.review, book.img_url
+        ])
+        console.log('Inserted record successfully!')
+    } catch (error) {
+        console.error('Error inserting record:', error);
+    }
+}
+
+async function getBookCover(title) {
+    const url = `https://openlibrary.org/search.json?title=`
+    const res = await axios.get(url + title +'&limit=1')
+    return res.data.docs[0].isbn[0]
+}
+
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const books = await getBooksFromDB()
     const data = {
         books: books
     };
@@ -35,6 +54,18 @@ app.get('/', (req, res) => {
 
 app.get('/add', (req, res) => {
     res.render('new')
+})
+
+app.post('/add', async (req, res) => {
+    const book = {
+        title: req.body.title.trim(),
+        rating: req.body.rating,
+        review: req.body.review
+    }
+    const book_isbn = await getBookCover(book.title)
+    book.img_url = `https://covers.openlibrary.org/b/isbn/${book_isbn}-M.jpg`
+    await postBookToDB(book)
+    res.redirect('/')
 })
 
 app.listen(PORT, () => {
